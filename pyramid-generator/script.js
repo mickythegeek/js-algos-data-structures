ec9aa486ad05c2fae955532ff88440d6bf055082
Michael David <lordkay07@gmail.com>
2025-01-02 17:35:11 +0100

    move the comparison into the while condition

--- pyramid-generator/script.js
+++ pyramid-generator/script.js
@@ -14,6 +14,13 @@ console.log(call);
 //     rows.push(padRow(i, count));
 // }
 
+let continueLoop = false;
+let done = 0;
+while(done !== count){
+    done++;
+    rows.push(padRow(done, count));
+}
+
 let result = "";
 
 for(const row of rows){
