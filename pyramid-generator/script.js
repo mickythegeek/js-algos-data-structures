873d48db9de04ce3025bae7c7a6aaf4a97234220
Michael David <lordkay07@gmail.com>
2025-01-02 18:12:13 +0100

    Clean up code

--- pyramid-generator/script.js
+++ pyramid-generator/script.js
@@ -1,7 +1,7 @@
 const character = "#";
 const count = 8;
 const rows = [];
-
+let inverted = true;
  
 function padRow(rowNumber, rowCount){
     return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
@@ -10,16 +10,15 @@ function padRow(rowNumber, rowCount){
 const call = padRow();
 console.log(call);
 
-// TODO: Use a different type of loop
 for(let i = 1; i <= count; i++){
-    rows.unshift(padRow(i, count));
+    if(inverted){
+        rows.unshift(padRow(i, count));
+    } else{
+        rows.push(padRow(i, count));
+    }
+    
 }
 
-/*
-while(rows.length < count){
-    rows.push(padRow(rows.length + 1, count));
-}
-*/
 
 for (let i = count; i > 0; i--){
     rows.push(padRow(i, count));
