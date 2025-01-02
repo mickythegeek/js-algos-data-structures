604c4bdb59bf9df7129c901dbe05ca630f38bcaf
Michael David <lordkay07@gmail.com>
2025-01-02 18:06:04 +0100

    Update your loop body to add new rows to the beginning of the array

--- pyramid-generator/script.js
+++ pyramid-generator/script.js
@@ -2,18 +2,18 @@ const character = "#";
 const count = 8;
 const rows = [];
 
-/* 
+ 
 function padRow(rowNumber, rowCount){
     return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
 }
-*/
+
 const call = padRow();
 console.log(call);
 
 // TODO: Use a different type of loop
-// for(let i = 1; i <= count; i++){
-//     rows.push(padRow(i, count));
-// }
+for(let i = 1; i <= count; i++){
+    rows.unshift(padRow(i, count));
+}
 
 /*
 while(rows.length < count){
