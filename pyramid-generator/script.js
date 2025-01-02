34261ec27b9338dc40d4e817d3611c921fbaa186
Michael David <lordkay07@gmail.com>
2025-01-02 08:52:13 +0100

    Update the character value to be repeated 2 * rowNumber - 1 times.

--- pyramid-generator/script.js
+++ pyramid-generator/script.js
@@ -3,14 +3,14 @@ const count = 8;
 const rows = [];
 
 function padRow(rowNumber, rowCount){
-    return character.repeat(rowNumber);
+    return ".".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + ".".repeat(rowCount - rowNumber);
 }
 
 const call = padRow();
 console.log(call);
 
 for(let i = 0; i < count; i++){
-    rows.push(character.repeat(i+1));
+    rows.push(padRow(i+1, count));
 }
 
 let result = "";
