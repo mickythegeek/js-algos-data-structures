dd6efe4ea4d9ec8c11b2bb311622ef426450e419
Michael David <lordkay07@gmail.com>
2025-01-02 06:19:04 +0100

    use special type of loop to manipulate the elements of an array

--- pyramid-generator/script.js
+++ pyramid-generator/script.js
@@ -3,8 +3,13 @@ const count = 8;
 const rows = [];
 
 for(let i = 0; i < count; i++){
-    rows.push(i);
+    rows.push(character.repeat(i+1));
 }
 
 let result = "";
-console.log(result);
+
+for(const row of rows){
+    result = result  + row + "\n";
+}
+
+console.log(result)
