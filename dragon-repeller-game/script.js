3a2666d5629b8bf291401ee2e47b3021a929dc40
Michael David <lordkay07@gmail.com>
2025-02-10 15:28:10 +0100

    Added store functionality

--- dragon-repeller-game/script.js
+++ dragon-repeller-game/script.js
@@ -20,6 +20,8 @@ const monsterHealthText = document.querySelector("#monsterHealth");
 function goStore(){
     // change text
     button1.innerText = "Buy 10 health (10 gold)";
+    button2.innerText = "Buy weapon (30 gold)";
+    button3.innerText = "Go to town square";
 
 }
 
