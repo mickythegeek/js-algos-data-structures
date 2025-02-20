63feef90ba81429606b30d2f9f05fa756f6c4abf
Michael David <lordkay07@gmail.com>
2025-02-20 10:49:20 +0100

    Add monster objects

--- dragon-repeller-game/script.js
+++ dragon-repeller-game/script.js
@@ -35,6 +35,25 @@ const weapons = [
     power: 100,
   },
 ];
+
+// add monster objects
+const monsters = [
+  {
+    name: "slime",
+    level: 2,
+    health: 15
+  },
+  {
+    name: "fanged beast",
+    level: 8,
+    health: 60
+  },
+  {
+    name: "dragon",
+    level: 20,
+    health: 300
+  }
+]
 const locations = [
   //add town square location object
   {
@@ -94,10 +113,6 @@ function goStore() {
 
 function goCave() {
   update(locations[2]);
-}
-
-function fightDragon() {
-  console.log("FIghting dragon.");
 }
 
 // create functions for buttons
@@ -160,3 +175,6 @@ function sellWeapon(){
 }
 function fightSlime() {}
 function fightBeast() {}
+function fightDragon() {
+  console.log("FIghting dragon.");
+}
