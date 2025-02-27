95b2e528751dc850c0864b6cf114d3f611ed635b
Michael David <lordkay07@gmail.com>
2025-02-27 13:15:09 +0100

    increase player gold and xp when he wins

--- dragon-repeller-game/script.js
+++ dragon-repeller-game/script.js
@@ -227,6 +227,7 @@ function lose(){
 }
 
 function win(){
-  gold += Math.floor(monsters[fighting].level * 6.7);
+  gold += Math.floor(monsters[fighting].level * 6.7); // increase player gold by monster level * 6.7
+  xp += monsters[fighting].level; // increase player xp by monster level
 
 }
