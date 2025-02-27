d37557a3de67101c10b59c12ce074065acac7994
Michael David <lordkay07@gmail.com>
2025-02-27 13:25:50 +0100

    display the updated values ot the player's gold and xp

--- dragon-repeller-game/script.js
+++ dragon-repeller-game/script.js
@@ -229,5 +229,7 @@ function lose(){
 function win(){
   gold += Math.floor(monsters[fighting].level * 6.7); // increase player gold by monster level * 6.7
   xp += monsters[fighting].level; // increase player xp by monster level
+  goldText.innerText = gold;
+  xpText.innerText = xp;
 
 }
