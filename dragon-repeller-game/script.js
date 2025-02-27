60c4d9ce155605ece20dcedc213793fc96f959b3
Michael David <lordkay07@gmail.com>
2025-02-27 15:58:23 +0100

    add each random number to text

--- dragon-repeller-game/script.js
+++ dragon-repeller-game/script.js
@@ -313,6 +313,11 @@ function pick(guess){
   while(numbers.length < 10){
     numbers.push(Math.floor(Math.random()* 11)) // add random number between 0 and 10 to numbers array
   }
+  text.innerText = "You picked " + guess + ". Here are the random numbers:\n";
+  for (let i=0; i<10;i++){
+    text.innerText += numbers[i] + "\n"; // add each random number to text
+  }
+
 }
 
 function pickTwo(){
