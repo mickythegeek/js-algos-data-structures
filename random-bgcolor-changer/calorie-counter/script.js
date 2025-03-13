5ad2c5aa9a55ab084620a0af9eaefc8adef410a4
Michael David <lordkay07@gmail.com>
2025-03-13 18:09:13 +0100

    remove console log from cleanInputString

--- random-bgcolor-changer/calorie-counter/script.js
+++ random-bgcolor-changer/calorie-counter/script.js
@@ -7,9 +7,10 @@ const output = document.getElementById('output');
 let isError = false;
 
 function cleanInputString(str){
-    console.log("original string: ", str)
     const regex = /[+-\s]/g;
     return str.replace(regex, "");
 }
 
-console.log(cleanInputString("+-99"));
+function isInvalidInput(str){
+    
+}
