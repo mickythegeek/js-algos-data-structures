705030c3796ebb31a30f4d974d372f27854f3e07
Michael David <lordkay07@gmail.com>
2025-03-13 12:49:19 +0100

    Get the span element

--- random-bgcolor-changer/script.js
+++ random-bgcolor-changer/script.js
@@ -20,4 +20,6 @@ function getRandomIndex(){
 
 getRandomIndex();
 
-const body = document.querySelector("body"); 
+const body = document.querySelector("body");
+const bgHexCodeSpanElement = document.querySelector("#bg-hex-code");
+console.log(bgHexCodeSpanElement); 
