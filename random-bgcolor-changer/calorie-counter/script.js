29e4aba9ef4a011efabd286a2f531f557722265e
Michael David <lordkay07@gmail.com>
2025-03-14 19:52:42 +0100

    Add function to dynamically add entry fields and attach event listener to button

--- random-bgcolor-changer/calorie-counter/script.js
+++ random-bgcolor-changer/calorie-counter/script.js
@@ -17,4 +17,16 @@ function isInvalidInput(str){
 
 }
 
-console.log(entryDropdown.value)
+function addEntry(){
+    const targetInputContainer = document.querySelector(`#${entryDropdown.value} .input-container`);
+    const entryNumber = targetInputContainer.querySelectorAll('input[type="text"]').length;
+    const HTMLString = `
+    <label for="${entryDropdown.value}-${entryNumber}-name">Entry ${entryNumber} Name</label>
+    <input type="text" placeholder="Name" id="${entryDropdown.value}-${entryNumber}-name" />
+    <label for="${entryDropdown.value}-${entryNumber}-calories">Entry ${entryNumber} Calories</label>
+    <input type="number" min="0" placeholder="Calories" id="${entryDropdown.value}-${entryNumber}-calories"  />
+    `;
+    targetInputContainer.innerHTML += HTMLString;
+}
+
+addEntryButton.addEventListener('click', addEntry); //
