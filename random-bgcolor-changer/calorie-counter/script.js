9805c6c29abf1c379152b7e0f92fa80e8e4fa585
Michael David <lordkay07@gmail.com>
2025-03-14 23:25:20 +0100

    Add calculation for remaining calories in calculateCalories function

--- random-bgcolor-changer/calorie-counter/script.js
+++ random-bgcolor-changer/calorie-counter/script.js
@@ -49,6 +49,7 @@ function calculateCalories(e){
     }
 
     const consumedCalories = breakfastCalories + lunchCalories + dinnerCalories + snacksCalories;
+    const remainingCalories = budgetCalories - consumedCalories + exerciseCalories;
 }
 
 function getCaloriesFromInputs(list){
