df5f13d6f568f768b9a124f6624c21aba065511f
Michael David <lordkay07@gmail.com>
2025-03-15 00:09:04 +0100

    Clear budget number input field after form submission

--- random-bgcolor-changer/calorie-counter/script.js
+++ random-bgcolor-changer/calorie-counter/script.js
@@ -83,6 +83,7 @@ function clearForm(){
     for( const container of inputContainers){
         container.innerHTML = "";
     }
+    budgetNumberInput.value = "";
 }
 
 addEntryButton.addEventListener('click', addEntry);
