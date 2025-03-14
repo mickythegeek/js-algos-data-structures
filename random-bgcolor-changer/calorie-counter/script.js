c71dc81cabd0612f371987bc42a726183f1d0b5b
Michael David <lordkay07@gmail.com>
2025-03-14 22:28:08 +0100

    Add error handling for invalid input in getCaloriesFromInput function

--- random-bgcolor-changer/calorie-counter/script.js
+++ random-bgcolor-changer/calorie-counter/script.js
@@ -36,6 +36,8 @@ function getCaloriesFromInput(list){
         const invalidInputMatch = isInvalidInput(currVal);
         if(invalidInputMatch){
             alert(`Invalid Input: ${invalidInputMatch[0]}`);
+            isError = true;
+            return null;
 
         }
 
