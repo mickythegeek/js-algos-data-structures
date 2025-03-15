36b35441ad40b279d2ddbc56c8a06ae69288916d
Michael David <lordkay07@gmail.com>
2025-03-15 08:39:40 +0100

    Add function to determine if the player has won the round in Rock-Paper-Scissors

--- rps-game/script.js
+++ rps-game/script.js
@@ -4,4 +4,17 @@ function getRandomComputerResult(){
     return options[randomIndex];
 
 }
-console.log(getRandomComputerResult());
+
+function hasPlayerWonTheRound(player, computer){
+    if(player === "Rock" && computer === "Scissors"){
+        return true;
+    } else if(player === "Scissors" && computer === "Paper"){
+        return true;
+    } else if(player === "Paper" && computer === "Rock"){
+        return true;
+    } else if(player === computer){
+        return false;
+    } else{
+        return false;
+    }
+}
